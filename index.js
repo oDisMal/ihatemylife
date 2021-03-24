11872b4bd47f3d0d2be197683fa2f9830306763a
oDisMal <57813004+oDismal@users.noreply.github.com>
2021-03-23 22:54:13 -0400

    made animations faster n shit

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ function showabout(){
     $("#about_container").addClass("animated slideInLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideInLeft");
-    },800);
+    },500);
 }
 function closeabout(){
     $("#about_container").addClass("animated slideOutLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideOutLeft");
         $("#about_container").css("display","none");
-    },800);
+    },500);
 }
 function showwork(){
     $("#work_container").css("display","inherit");
     $("#work_container").addClass("animated slideInRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideInRight");
-    },800);
+    },500);
 }
 function closework(){
     $("#work_container").addClass("animated slideOutRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideOutRight");
         $("#work_container").css("display","none");
-    },800);
+    },500);
 }
 // function showcontact(){
 //     $("#contact_container").css("display","inherit");
@@ -41,13 +41,10 @@ function closework(){
 //     },800);
 // }
 setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
       $("#box").css("display","none");
       $("#about").removeClass("animated fadeIn");
     //   $("#contact").removeClass("animated fadeIn");
       $("#work").removeClass("animated fadeIn");
-    },1000);
+    },1500);
 },1500);
